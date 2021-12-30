6a8b2668063628902f4c352f0d56b00c1a18a941
bram <bram@pop-os.localdomain>
2021-12-30 15:52:46 +0100

    added functionality for a header in the homelist (so that the user can categorize by day, for instance), the header does not show in triplist or supermarketlist. The code is functional, but ugly

--- src/lib/shop-list-entry.dart
+++ src/lib/shop-list-entry.dart
@@ -6,31 +6,96 @@ import 'package:flutter/material.dart';
 
 import 'util.dart';
 
+/* class Entry {
+  String text;
+  Entry({this.text=""})
+}
+
+class CheckListProductEntry extends Entry {
+  // TODO: inherit from superclass
+  // define how a tile looks like in homelist, triplist and in supermarketorder
+}
+
+class CheckListProductInputField extends Entry {
+  // TODO: inherit from superclass
+  // define how a tile looks like in homelist, triplist and in supermarketorder
+}
+
+class CheckListHeaderEntry extends Entry {
+  // TODO: inherit from superclass
+  // define how a tile looks like in homelist, triplist and in supermarketorder
+}
+
+class CheckListHeaderInputField extends Entry {
+  // TODO: inherit from superclass
+  // define how a tile looks like in homelist, triplist and in supermarketorder
+}
+
+class SuperMarketOrderEntry extends Entry {
+
+}
+
+class SuperMarketOrderInputField extends Entry {
+
+} */
+
 class ShopListEntry{
     String productName;
     bool inShopList;
     bool checkedOff;
+    bool isHeader; // TODO: wouldn't it be better if there is a separate class for listItemHeader and listItemProduct or something?
+    bool isHeaderInputField; // TODO: separate classes (subclasses?) for these purposes
+    bool isEntryInputField; // TODO: separate classes for these purposes
     int  homeIndex;
     int  supermarketIndex;
 
-    ShopListEntry({@required this.productName, this.inShopList=true, this.checkedOff=false, this.homeIndex=-1, this.supermarketIndex=-1});
+    ShopListEntry({@required this.productName, this.inShopList=true, this.checkedOff=false, this.isHeader=false, this.isHeaderInputField=false, this.isEntryInputField=false, this.homeIndex=-1, this.supermarketIndex=-1});
 
     String getReducedProductName() => reduceProductName(productName);
 
     ShopListEntry.fromJson(Map<String, dynamic> json):
-        productName      = json['productName'],
-        inShopList       = json['inShopList'],
-        checkedOff       = json['checkedOff'],
-        homeIndex        = json['homeIndex'],
-        supermarketIndex = json['supermarketIndex']
+        productName        = json['productName'],
+        inShopList         = json['inShopList'],
+        checkedOff         = json['checkedOff'],
+        isHeader           = json['isHeader'],
+        isHeaderInputField = json['isHeaderInputField'],
+        isEntryInputField  = json['isEntryInputField'],
+        homeIndex          = json['homeIndex'],
+        supermarketIndex   = json['supermarketIndex']
     ;
 
     Map<String, dynamic> toJson() => {
-        'productName'      : productName,
-        'inShopList'       : inShopList,
-        'checkedOff'       : checkedOff,
-        'homeIndex'        : homeIndex,
-        'supermarketIndex' : supermarketIndex,
+        'productName'        : productName,
+        'inShopList'         : inShopList,
+        'checkedOff'         : checkedOff,
+        'isHeader'           : isHeader,
+        'isHeaderInputField' : isHeaderInputField,
+        'isEntryInputField'  : isEntryInputField,
+        'homeIndex'          : homeIndex,
+        'supermarketIndex'   : supermarketIndex,
     };
 
+    String hintText() {
+      if (isHeaderInputField) {
+        return "tap to create new header...";
+      } else if (isEntryInputField) {
+        return "tap to create new entry...";
+      } else {
+        return "BUG! this text should not be here";
+      }
+    }
+}
+
+class ShopListEntrySubClass extends ShopListEntry {
+    String productName;
+    bool inShopList;
+    bool checkedOff;
+    bool isHeader; // TODO: wouldn't it be better if there is a separate class for listItemHeader and listItemProduct or something?
+    bool isHeaderInputField; // TODO: separate classes (subclasses?) for these purposes
+    bool isEntryInputField; // TODO: separate classes for these purposes
+    int  homeIndex;
+    int  supermarketIndex;
+    bool isSubClass;
+
+    ShopListEntrySubClass({@required this.productName, this.inShopList=true, this.checkedOff=false, this.isHeader=false, this.isHeaderInputField=false, this.isEntryInputField=false, this.homeIndex=-1, this.supermarketIndex=-1, this.isSubClass=true});
 }
